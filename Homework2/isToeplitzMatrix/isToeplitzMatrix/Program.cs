8cd2588c507a0e5bd7d0021c5d4782445f5a7784
JPForever7 <2959615007@qq.com>
2022-02-25 10:56:59 +0800

    Homework2

---
+++ Homework2/isToeplitzMatrix/isToeplitzMatrix/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace isToeplitzMatrix
+{
+    class Program
+
+    {
+        public bool isToep(int[,] array,int row,int list)
+        {
+            bool flag = true;
+            for (int i= 1;i<row;i++)
+            {
+                for(int j= i;j<list;j++)
+                {
+                    if (array[i, j] != array[i - 1, j - 1]) flag = false;
+                }
+            }
+            return flag;
+        }
+        static void Main(string[] args)
+        {
+            //input matrix
+            Console.Write("input row number and  list");
+            int row = int.Parse(Console.ReadLine());
+            int list = int.Parse(Console.ReadLine());
+            int[,] array = new int[row,list];
+            for(int i= 0;i<row;i++)
+            {
+                for(int j = 0;j<list;j++)
+                {
+                    array[i, j] = int.Parse(Console.ReadLine());
+
+                }
+            }
+            Program a = new Program();
+            Console.WriteLine(a.isToep(array, row, list));
+
+
+        }
+    }
+}
